ebeff4543acc260839cf7f977c75ee4bb9a25338
hernangit config --global user.name John <hernan0066@gmail.com>
2021-11-15 23:50:49 -0300

    modificaciones newsletter

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Newsletter = () => {
     return (
         <div className="news__main">
+            
             <h3>Newsletter</h3>
             <p>Recibí en tu mail novedades, promociones, ofertas especiales y todas nuestras noticias.</p>
             <div className="news__mail">
