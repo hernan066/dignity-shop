efeb3e83ebd7dc1f3f288ecc56c16438ce83b31d
hernan066 <hernan0066@gmail.com>
2021-11-19 05:30:08 -0300

    detalle responsive

--- src/pages/Detalle.jsx
+++ src/pages/Detalle.jsx
@@ -1,50 +1,54 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
 
 const Detalle = () => {
-    return (
-        <>
-        <Navbar />
-        <div className="detalle__main">
-            
-            <div className="detalle__img">
-            <img src="https://i.ibb.co/0jsbxCY/1-2.jpg" alt="3" border="0"/>
-            </div>
-            <div className="detalle__info">
-               <h1>Short Modal</h1>
-              
-               <div className="detalle__review">
-               <i className="far fa-star"></i>
-               <i className="far fa-star"></i>
-               <i className="far fa-star"></i>
-               <i className="far fa-star"></i>
-               <i className="far fa-star"></i>
-               <p>0 review(s)</p>
-               
-               </div>
-               
-               
-               <h2>$300</h2>
-               <h3>Detalle del producto</h3>
-               <p>Short de modal de niño</p>
-               
-                <h3>Talle</h3>
-                <div className="detalle__talle">
-                <span>S</span> <span>M</span><span>L</span>
-                </div>
-                <h3>Metodo de pago</h3>
-                <p>Efectivo - Trasferencia bancaria - Marcado pago</p>
-                <h3>Metodo de envio</h3>
-                <p>Consultar</p>
-               
-               
-               <button>Agregar al carrito</button>
-            </div>
-
-            
+  return (
+    <>
+      <Navbar />
+      <div className="detalle__main">
+        <div className="detalle__img">
+          <img src="https://i.ibb.co/722pd72/1.jpg" alt="3" border="0" />
         </div>
-        </>
-    )
-}
+        <div className="detalle__info">
+          <h1>Short Modal</h1>
 
-export default Detalle
+          <div className="detalle__review">
+            <i className="far fa-star"></i>
+            <i className="far fa-star"></i>
+            <i className="far fa-star"></i>
+            <i className="far fa-star"></i>
+            <i className="far fa-star"></i>
+            <p>0 review(s)</p>
+          </div>
+          <div className="detalle__infos">
+            <h3>Precio</h3>
+            <p>$300</p>
+          </div>
+          <div className="detalle__infos">
+            <h3>Detalle del producto</h3>
+            <p>Short de modal de niño</p>
+          </div>
+          <div className="detalle__infos">
+            <h3>Talle</h3>
+            <div className="detalle__talle">
+              <span>S</span> <span>M</span>
+              <span>L</span>
+            </div>
+          </div>
+          <div className="detalle__infos">
+            <h3>Metodo de pago</h3>
+            <p>Efectivo - Trasferencia bancaria - Marcado pago</p>
+          </div>
+          <div className="detalle__infos">
+            <h3>Metodo de envio</h3>
+            <p>Consultar</p>
+          </div>
+
+          <button>Agregar al carrito</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Detalle;
