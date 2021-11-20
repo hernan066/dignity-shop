e2eff7bb476944a1eeba65ff79fdfdc38c345eaf
hernan066 <hernan0066@gmail.com>
2021-11-20 01:54:28 -0300

    redux instalado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import AppRouter from "./router/AppRouter";
+import { store } from './store/store';
 
 
 
 function App() {
   return (
-   <AppRouter />
+    <Provider store={store}>
+     <AppRouter />
+  </Provider>
     
   );
 }
---
+++ src/reducers/rootReducers.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+
+
+
+export const rootReducer = combineReducers({
+    /* ui: uiReducer,
+    calendar: calendaReducer,
+    auth: authReducer, */
+});
