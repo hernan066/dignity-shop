a5ada561de9f778852bc9c5fcc4daa5c5afdb9b0
hernan066 <hernan0066@gmail.com>
2021-11-25 04:47:31 -0300

    letras producto

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -5,19 +5,24 @@ const producto = ({datos}) => {
   
   
   return (
-    <Link to={`/producto/${datos._id}`}>
+    
         
     <div className="prod__grid">
+      
+      <Link to={`/producto/${datos._id}`}>
       <div className="prod__hover">
+        
         <img src={datos.img} alt="1" border="0" />
+        
         <div className="prod__info">
           <p> {datos.title}</p>
 
           <p>${datos.price}</p>
         </div>
       </div>
-    </div>
         </Link>
+    </div>
+        
   );
 };
 
